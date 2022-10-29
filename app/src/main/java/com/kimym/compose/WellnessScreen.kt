dc4945d0da8f938bee14e80e1d4e930b37e01309
kym1924 <kym1924@naver.com>
2022-10-30 02:21:51 +0900

    State in ViewModel

--- app/src/main/java/com/kimym/compose/WellnessScreen.kt
+++ app/src/main/java/com/kimym/compose/WellnessScreen.kt
@@ -2,36 +2,24 @@ package com.kimym.compose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-
-/*
- * Don't do this!
- * val list = remember { mutableStateListOf()<WellnessTask>() }
- * list.addAll(getWellnessTasks())
- * It might result in unexpected recomposition and suboptimal UI performance.
- * It would result in duplicated items being added for every recomposition.
- *
- * Do this instead.
- * val list = remember {
- * mutableStateListOf<WellnessTask>().apply { addAll(getWellnessTasks()) }
- */
-
-/*
- * If you try to use 'rememberSaveable()' to store the list,
- * you'll get a runtime exception.
- * You shouldn't be using rememberSaveable to store large amounts of data or
- * complex data structures that require lengthy serialization or deserialization.
- */
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun WellnessScreen(modifier: Modifier = Modifier) {
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel(),
+) {
     Column(modifier = modifier) {
         StatefulCounter()
-        val list = remember { getWellnessTasks().toMutableStateList() }
-        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
--- app/src/main/java/com/kimym/compose/WellnessTaskItem.kt
+++ app/src/main/java/com/kimym/compose/WellnessTaskItem.kt
@@ -9,31 +9,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kimym.compose.ui.theme.StateInComposeTheme
 
-@Composable
-fun WellnessTaskItem(
-    taskName: String,
-    onClose: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier
-    )
-}
+/*
+ * We no longer need a stateful method.
+ * As the CheckBox state will be hoisted to the List level.
+ */
 
 @Composable
 fun WellnessTaskItem(
