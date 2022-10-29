df15ac8e4dd990d7ae4f59b6ec28903288d372f4
kym1924 <kym1924@naver.com>
2022-10-30 02:11:13 +0900

    Observable MutableList

--- app/src/main/java/com/kimym/compose/WellnessScreen.kt
+++ app/src/main/java/com/kimym/compose/WellnessScreen.kt
@@ -2,12 +2,36 @@ package com.kimym.compose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+
+/*
+ * Don't do this!
+ * val list = remember { mutableStateListOf()<WellnessTask>() }
+ * list.addAll(getWellnessTasks())
+ * It might result in unexpected recomposition and suboptimal UI performance.
+ * It would result in duplicated items being added for every recomposition.
+ *
+ * Do this instead.
+ * val list = remember {
+ * mutableStateListOf<WellnessTask>().apply { addAll(getWellnessTasks()) }
+ */
+
+/*
+ * If you try to use 'rememberSaveable()' to store the list,
+ * you'll get a runtime exception.
+ * You shouldn't be using rememberSaveable to store large amounts of data or
+ * complex data structures that require lengthy serialization or deserialization.
+ */
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
