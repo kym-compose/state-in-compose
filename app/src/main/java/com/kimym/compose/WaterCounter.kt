9613c1ed32522134b0cbf2632b47a4535473ff06
kym1924 <kym1924@naver.com>
2022-10-30 02:03:50 +0900

    State hoisting

--- app/src/main/java/com/kimym/compose/WaterCounter.kt
+++ app/src/main/java/com/kimym/compose/WaterCounter.kt
@@ -12,18 +12,44 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+/*
+ * Your stateless composable can now be reused.
+ * @Composable
+ * fun StatefulCounter() {
+ *     var waterCount by remember { mutableStateOf(0) }
+ *     var juiceCount by remember { mutableStateOf(0) }
+ *     StatelessCounter(count = waterCount, onIncrement = { waterCount++ })
+ *     StatelessCounter(count = juiceCount, onIncrement = { juiceCount++ })
+ * }
+ */
+
+/*
+ * Your stateful composable function can provide the same state to multiple composable functions.
+ * @Composable
+ * fun StatefulCounter() {
+ *     var count by remember { mutableStateOf(0) }
+ *     StatelessCounter(count, { count++ })
+ *     AnotherStatelessMethod(count, { count *= 2 })
+ * }
+ */
+
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(
+    count: Int,
+    onIncrement: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Column(modifier = modifier.padding(16.dp)) {
-        /**
-         * Changes to count are now tracked by Compose.
-         * val count: MutableState<Int> = remember { mutableStateOf(0) }
-         */
-        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text(text = "You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
